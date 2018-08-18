f6dd8486bb76af4e8fd584453ae8720015469aed
Rommel C. Montilus <rommel@Rommels-MacBook-Air.local>
2018-08-18 12:51:47 -0400

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'awesome' data
+var friendList = [
+    {
+      name: 'Anthony Fantano',
+      profilePic: 'https://i.ytimg.com/vi/SCLwyNT25bU/hqdefault.jpg',
+      scores: [
+        "1",
+        "1",
+        "5",
+        "3",
+        "5",
+        "5",
+        "4",
+        "3",
+        "5",
+        "2"
+      ]
+    },
+    {
+      name: 'Bill Burr',
+      profilePic: 'https://bloximages.newyork1.vip.townnews.com/ocolly.com/content/tncms/assets/v3/editorial/2/61/261ecac2-33a2-11e8-9d58-fbf80d686195/5abd6b5e2d5c8.image.jpg',
+      scores: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "1",
+        "2",
+        "1",
+        "2",
+        "4",
+        "4"
+
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
